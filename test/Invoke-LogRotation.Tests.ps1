51a3540078f811f38709224e6fd1d0f9ea5dfa39
Trent Willingham <TrentWillingham@gmail.com>
2017-02-16 01:34:28 -0500

    Release v.0.2.0 (#2)
    * Update versioning
    
    * Fix test errors

--- test/Invoke-LogRotation.Tests.ps1
+++ test/Invoke-LogRotation.Tests.ps1
@@ -4,45 +4,45 @@ $SuppressImportModule = $false
 
 InModuleScope -ModuleName 'ServerManagementTools' {
     Describe 'Invoke-LogRotation' {
-        In $TestDrive {
-            $TestPath = 'Logs\W3SVC1'
-            $TestFullPath = Join-Path $TestDrive $TestPath
-            Setup -Path $TestPath -Dir
 
-            for ( $i=0; $i -le 10; $i++ ) {
-                $FileDate = (Get-Date).AddDays(-$i)
-                $LabelDate = $FileDate.ToString('yyyyMMdd')
-                (Setup -File "$TestPath\ex_$LabelDate.log" -Content "Test" -PassThru | Get-Item).LastWriteTime = $FileDate
+        Context 'Mock unit tests' {
+            Mock -CommandName Test-Path -MockWith { $true }
+            Mock -CommandName Get-ChildItem -MockWith { }
+
+            It 'Calls Test-Path to verify $Path exists' {
+                Invoke-LogRotation -Path 'Path1'
+                Assert-MockCalled -CommandName 'Test-Path' -Scope 'It' -Times 1 -Exactly
             }
 
-            Context 'Mock unit tests' {
-                It 'Calls Test-Path to verify $Path exists' {
-                    Mock -CommandName Test-Path { $true }
-                    Invoke-LogRotation -Path 'Path1'
-                    Assert-MockCalled -CommandName 'Test-Path' -Scope 'It' -Times 1 -Exactly
-                }
-
-                It 'Accepts array of Path values' {
-                    Mock -CommandName Test-Path { $true }
-                    Invoke-LogRotation -Path 'Path2','Path3'
-                    Assert-MockCalled -CommandName 'Test-Path' -Scope 'It' -Times 2 -Exactly
-                }
-
-                It 'Calls Get-ChildItem to retrive log files in $Path' {
-                    Mock -CommandName Test-Path { $true }
-                    Mock -CommandName Get-ChildItem { }
-                    Invoke-LogRotation -Path 'Path5' -CompressDays 3
-                    Assert-MockCalled -CommandName Get-ChildItem -Scope 'It' -Times 1 -Exactly
-                }
-
-                It 'Throws error if Test-Path fails' {
-                    Mock -CommandName Test-Path { $false }
-                    Invoke-LogRotation -Path 'Path4' -EA SilentlyContinue -EV ErrOut
-                    $ErrOut.Exception | Should Match "Cannot find path"
-                }
+            It 'Accepts array of Path values' {
+                Invoke-LogRotation -Path 'Path2','Path3'
+                Assert-MockCalled -CommandName 'Test-Path' -Scope 'It' -Times 2 -Exactly
             }
 
-            Context 'Integration test' {
+            It 'Calls Get-ChildItem to retrive log files in $Path' {
+                Invoke-LogRotation -Path 'Path5'
+                Assert-MockCalled -CommandName Get-ChildItem -Scope 'It' -Times 1 -Exactly
+            }
+
+            It 'Throws error if Test-Path fails' {
+                Mock -CommandName Test-Path { $false }
+                Invoke-LogRotation -Path 'Path4' -EA SilentlyContinue -EV ErrOut
+                $ErrOut.Exception | Should Match "Cannot find path"
+            }
+        }
+
+        Context 'Integration test' {
+            In $TestDrive {
+                $TestPath = 'Logs\W3SVC1'
+                $TestFullPath = Join-Path $TestDrive $TestPath
+                Setup -Path $TestPath -Dir
+
+                for ( $i=0; $i -le 10; $i++ ) {
+                    $FileDate = (Get-Date).AddDays(-$i)
+                    $LabelDate = $FileDate.ToString('yyyyMMdd')
+                    (Setup -File "$TestPath\ex_$LabelDate.log" -Content "Test" -PassThru | Get-Item).LastWriteTime = $FileDate
+                }
+
                 $Return = Invoke-LogRotation -Path $TestFullPath -CompressDays 3
                 $LogFiles = Get-ChildItem -Path $TestFullPath -Include '*.log' -Recurse
                 $ZipFiles = Get-ChildItem -Path $TestFullPath -Include '*.zip' -Recurse
