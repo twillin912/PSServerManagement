458be09be6985b8d3386ae56fedb221fd73ad94a
Trent Willingham <TrentWillingham@gmail.com>
2018-04-24 14:37:14 -0400

    Refactor parameter name

--- ServerManagementTools/Public/Invoke-LogRotation.ps1
+++ ServerManagementTools/Public/Invoke-LogRotation.ps1
@@ -4,19 +4,11 @@ function Invoke-LogRotation {
         Compresses log files by month.
     .DESCRIPTION
         The Invoke-LogRotation cmdlet retrieves a list of log file in the specified locations and compressed them into a ZIP archive by month.  Once the contents of the archive are verified the original log files are deleted.
-    .PARAMETER Path
-        Specifies a path to one or more locations.  Invoke-LogRotation processes the log files in the specified locations.
-    .PARAMETER CompressDays
-        Specifies the number of days to keep uncompressed log files.  If you do not specify this parameter, the cmdlet will retain 5 days.
-    .PARAMETER Include
-
-    .PARAMETER Exclude
-
     .EXAMPLE
         Invoke-LogRotation -Path C:\Inetpub\Logs\LogFiles\W3SVC1
         Archives the log files for the IIS 'Default Website' using the default 5 day retention
     .EXAMPLE
-        Invoke-LogRotation -Path C:\Inetpub\Logs\LogFiles\W3SVC1 -CompressDays 10
+        Invoke-LogRotation -Path C:\Inetpub\Logs\LogFiles\W3SVC1 -KeepRaw 10
         Archives the log files for the IIS 'Default Website' using the specified 10 day retention
     .LINK
         http://servermanagementtools.readthedocs.io/en/stable/functions/Invoke-LogRotation
@@ -25,94 +17,99 @@ function Invoke-LogRotation {
         Check out my other projects on GitHub https://github.com/twillin912
     #>
     [CmdletBinding(SupportsShouldProcess = $true)]
-    Param (
+    param(
+        # Specifies a path to one or more locations.  Invoke-LogRotation processes the log files in the specified locations.
         [Parameter(Mandatory = $true, Position = 1)]
-        [string[]] $Path,
+        [string[]]$Path,
 
+        # Specifies the number of days to keep uncompressed log files.  If you do not specify this parameter, the cmdlet will retain 5 days.
         [Parameter(Position = 2)]
-        [int] $CompressDays = 5,
+        [Alias('CompressDays')]
+        [int] $KeepRaw = 5,
 
+        # Specifies a wildcard selection string of files to include.
         [Parameter()]
-        [string] $Include = '*.log',
+        [string]$Include = '*.log',
 
+        # Specifies a wildcard selection string of files to exclude.
         [Parameter()]
-        [string] $Exclude
+        [string]$Exclude
     )
 
-    Begin {
+    begin {
         $DateDisplayFormat = 'MM/dd/yyyy'
         $DateFileFormat = 'yyyy-MM'
         $CurrentDate = Get-Date -Hour 0 -Minute 0 -Second 0
 
-        if ($CompressDays) {
-            $CompressBefore = (Get-Date -Date $CurrentDate).AddDays( - $CompressDays)
+        if ($KeepRaw) {
+            $CompressBefore = (Get-Date -Date $CurrentDate).AddDays( - $KeepRaw)
         }
 
-        $AdditionalParams = @{'Include' = $Include}
-        if ( $Exclude ) { $AdditionalParams.Add('Exclude', $Exclude)}
+        $AdditionalParams = @{ 'Include' = $Include }
+        if ($Exclude) { $AdditionalParams.Add('Exclude', $Exclude) }
 
-        $null = [Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" )
+        $null = [Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
 
     }
 
-    Process {
-        foreach ( $LogPath in $Path ) {
-            if ( ! ( Test-Path -Path $LogPath ) ) {
+    process {
+        foreach ($LogPath in $Path) {
+            if (!(Test-Path -Path $LogPath)) {
                 Write-Error -Message "Cannot find path '$LogPath' because it does not exist."
                 break
             }
 
             $LogFolder = Split-Path -Path $($LogPath) -Leaf
 
-            if ( $CompressDays ) {
+            if ($KeepRaw) {
                 $LogsToCompress = Get-ChildItem -Path $LogPath @AdditionalParams -Recurse |
                     Where-Object { $PSItem.PSIsContainer -eq $false -and $PSItem.LastWriteTime -lt $CompressBefore }
                 Write-Verbose -Message "Compressing $($LogsToCompress.Count) older than $($CompressBefore.ToString($DateDisplayFormat))"
 
                 $LogHashTable = @{}
-                foreach ( $File in $LogsToCompress ) {
+                foreach ($File in $LogsToCompress) {
                     $LogHashTable.Add($File.FullName, $File.LastWriteTime.ToString($DateFileFormat))
                 }
                 $LogHashTable = $LogHashTable.GetEnumerator() | Sort-Object -Property Value, Name
-                $MonthsToProcess = @( $LogHashTable | Group-Object -Property Value | Select-Object -Property Name )
+                $MonthsToProcess = @( $LogHashTable | Group-Object -Property Value | Select-Object -Property Name)
 
-                foreach ( $Month in $MonthsToProcess ) {
+                foreach ($Month in $MonthsToProcess) {
                     $ZipFileName = "$($env:ComputerName)-$($LogFolder)-$($Month.Name).zip"
                     $ZipFullName = Join-Path -Path $LogPath -ChildPath $ZipFileName
                     $CurrentMonthLogs = $LogHashTable | Where-Object { $PSItem.Value -eq "$($Month.Name)" }
 
-                    foreach ( $LogFile in $CurrentMonthLogs ) {
+                    foreach ($LogFile in $CurrentMonthLogs) {
                         $LogName = Split-Path -Path $LogFile.Name -Leaf
 
-                        if ( $PSCmdlet.ShouldProcess($ZipFullName, "Create/Update Archive") ) {
+                        if ($PSCmdlet.ShouldProcess($ZipFullName, "Create/Update Archive")) {
                             $ZipFile = [System.IO.Compression.ZipFile]::Open($ZipFullName, "Update")
                         }
 
-                        if ( $PSCmdlet.ShouldProcess($LogFile.Name, "Get Content") ) {
+                        if ($PSCmdlet.ShouldProcess($LogFile.Name, "Get Content")) {
                             $LogContent = Get-Content -LiteralPath $LogFile.Name -Raw
                         }
 
-                        if ( ! ( $ZipFile.GetEntry($LogName) ) ) {
-                            if ( $PSCmdlet.ShouldProcess($LogFile.Name, "Add to Archive") ) {
+                        if (!($ZipFile.GetEntry($LogName))) {
+                            if ($PSCmdlet.ShouldProcess($LogFile.Name, "Add to Archive")) {
                                 $ZipFileEntry = $ZipFile.CreateEntry($LogName)
-                                $StreamWriter = [System.IO.StreamWriter] $ZipFileEntry.Open()
+                                $StreamWriter = [System.IO.StreamWriter]$ZipFileEntry.Open()
                                 $StreamWriter.Write($LogContent)
                                 $StreamWriter.Dispose()
                                 $ZipFileEntry.LastWriteTime = (Get-Item -LiteralPath "$($LogFile.Name)").LastWriteTime
                             }
                         }
 
-                        if ( $PSCmdlet.ShouldProcess($ZipFullName, "Save Archive") ) {
+                        if ($PSCmdlet.ShouldProcess($ZipFullName, "Save Archive")) {
                             $ZipFile.Dispose()
                         }
 
-                        if ( $PSCmdlet.ShouldProcess($LogFile.Name, "Compare to Archive") ) {
+                        if ($PSCmdlet.ShouldProcess($LogFile.Name, "Compare to Archive")) {
                             $ZipFile = [System.IO.Compression.ZipFile]::Open($ZipFullName, "Read")
-                            $ZipFileEntry = [System.IO.StreamReader] $ZipFile.GetEntry($LogName).Open()
+                            $ZipFileEntry = [System.IO.StreamReader]$ZipFile.GetEntry($LogName).Open()
                             $ZipContent = $ZipFileEntry.ReadToEnd()
                             $ZipFile.Dispose()
 
-                            if ( $ZipContent -eq $LogContent -or $LogContent.Length -eq 0 ) {
+                            if ($ZipContent -eq $LogContent -or $LogContent.Length -eq 0) {
                                 Remove-Item -LiteralPath $LogFile.Name
                             }
                         }
@@ -123,7 +120,7 @@ function Invoke-LogRotation {
         }
     }
 
-    End {
+    end {
 
     }
 }
--- ServerManagementTools/Public/Register-LogRotationTask.ps1
+++ ServerManagementTools/Public/Register-LogRotationTask.ps1
@@ -13,48 +13,49 @@ function Register-LogRotationTask {
     General notes
     #>
     [CmdletBinding()]
-    Param (
-        # Parameter help description
+    param(
+        # Specifies the name of the scheduled task with ' - LogRotation' appended.
         [Parameter(Mandatory, Position = 1)]
         [string] $Name,
 
-        #
+        # Specifies a path to one or more locations.  Invoke-LogRotation processes the log files in the specified locations.
         [Parameter(Mandatory, Position = 2)]
-        [string] $Path,
+        [string[]] $Path,
 
-        #
+        # Specifies the number of days to keep uncompressed log files.  If you do not specify this parameter, the cmdlet will retain 5 days.
         [Parameter(Position = 3)]
-        [int] $CompressDays,
+        [Alias('CompressDays')]
+        [int] $KeepRaw,
 
-        #
+        # Specifies the start time for the scheduled task.  The default value is 10:00 PM.
         [Parameter()]
         [string] $StartTime = '22:00',
 
-        #
+        # Specifies a wildcard selection string of files to include.
         [Parameter()]
-        [string] $Include,
+        [string]$Include,
 
-        #
+        # Specifies a wildcard selection string of files to exclude.
         [Parameter()]
-        [string] $Exclude
+        [string]$Exclude
     )
 
     $Command = "Invoke-LogRotation -Path '$Path'"
-    if ( $CompressDays ) {
-        $Command += " -CompressDays $CompressDays"
+    if ($KeepRaw) {
+        $Command += " -KeepRaw $KeepRaw"
     }
-    if ( $Include ) {
+    if ($Include) {
         $Command += " -Include '$Include'"
     }
-    if ( $Exclude ) {
+    if ($Exclude) {
         $Command += " -Exclude '$Exclude'"
     }
 
     $TaskParams = @{
-        TaskName    = "LogRotation - $Name"
-        Trigger     = New-ScheduledTaskTrigger -At $StartTime -Daily
-        User        = 'NT AUTHORITY\SYSTEM'
-        Action      = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-NonInteractive -NoProfile -WindowStyle Hidden -Command `"$Command`""
+        TaskName = "LogRotation - $Name"
+        Trigger  = New-ScheduledTaskTrigger -At $StartTime -Daily
+        User     = 'NT AUTHORITY\SYSTEM'
+        Action   = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-NonInteractive -NoProfile -WindowStyle Hidden -Command `"$Command`""
     }
     Register-ScheduledTask @TaskParams -Force
 }
